6830535c0cae9bd927c366ce3c20a99b271b5d2d
Viktor Svertoka <victor.svertoka@gmail.com>
2024-12-13 00:48:05 +0200

    Create models.py

---
+++ src/database/models.py
@@ -0,0 +1,19 @@
+from datetime import date
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
+from sqlalchemy.sql.sqltypes import Date
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Note(Base):
+    __tablename__ = "contacts"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    surname: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50))
+    phone: Mapped[str] = mapped_column(String(50))
+    birthday: Mapped[date] = mapped_column(Date, nullable=False)
+    notes: Mapped[str] = mapped_column(String(300), nullable=True)
