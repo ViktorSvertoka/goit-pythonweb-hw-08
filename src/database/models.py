1e683c7347f8d0a85a1047db693c6c8cb5fd3c07
ViktorSvertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-12-14 02:02:57 +0200

    Feat(Files) Added alembic

--- src/database/models.py
+++ src/database/models.py
@@ -1,19 +1,17 @@
 from datetime import date
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.sql.sqltypes import Date
 
-
-class Base(DeclarativeBase):
-    pass
+Base = declarative_base()
 
 
 class Contact(Base):
     __tablename__ = "contacts"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    name: Mapped[str] = mapped_column(String(50), nullable=False)
-    surname: Mapped[str] = mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(50))
-    phone: Mapped[str] = mapped_column(String(50))
-    birthday: Mapped[date] = mapped_column(Date, nullable=False)
-    notes: Mapped[str] = mapped_column(String(300), nullable=True)
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(50), nullable=False)
+    surname = Column(String(50), nullable=False)
+    email = Column(String(50))
+    phone = Column(String(50))
+    birthday = Column(Date, nullable=False)
+    notes = Column(String(300), nullable=True)
